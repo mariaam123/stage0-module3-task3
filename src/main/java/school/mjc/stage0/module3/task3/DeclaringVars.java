59890e1dd91d56bf15b0bc23f033e08678a3f244
mariam bakradze <93291984+mariaam123@users.noreply.github.com>
2023-07-30 03:59:56 +0400

    Update DeclaringVars.java

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -2,9 +2,10 @@ package school.mjc.stage0.module3.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 10;
-        int c = 10;
+        int a;
+        int b;
+        int c;
+        a = b = c = 10;
         System.out.println(c);
     }
 }
